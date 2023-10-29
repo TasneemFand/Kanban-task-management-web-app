67955c74938705f2cd266727ef4ea91a41cb3bac
Tasneem Fandakli <tasnimtepe@gmail.com>
2023-10-29 18:28:00 +0200

    add actions

---
+++ app/actions/getBoards.tsx
@@ -0,0 +1,28 @@
+import { db } from "@/lib/db";
+
+export interface Props {
+    userId?: string;
+}
+export default async function getBoards(params: Props) {
+  try {
+    if (!params.userId) {
+      return null;
+    }
+
+    const boards = await db.board.findMany({
+      where: {
+        userId: params.userId,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    const safeBoards = boards.map((board) => ({
+      ...board,
+      createdAt: board.createdAt.toISOString(),
+    }));
+    return safeBoards;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
