44c540f09f702b257c1ceb0a240e001b37ac6178
Tasneem Fandakli <tasnimtepe@gmail.com>
2023-10-29 18:30:41 +0200

    add api routes for register and create boards

---
+++ app/api/boards/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import { db } from "@/lib/db";
+
+export async function POST(request: Request) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const body = await request.json();
+  const { name, columns } = body;
+
+
+  const board = await db.board.create({
+    data: {
+      name,
+      userId: currentUser.id,
+    },
+  });
+
+  await db.column.createMany({
+    data: columns.map((col: string) => ({
+        name: col,
+        boardId: board.id
+    }))
+  })
+
+  return NextResponse.json(board);
+}
