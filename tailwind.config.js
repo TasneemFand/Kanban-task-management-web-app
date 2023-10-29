1b0b3e77c572fddcf95b35bf8a042942adb5653d
Tasneem Fandakli <tasnimtepe@gmail.com>
2023-10-29 18:20:13 +0200

    update tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,94 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
+  theme: {
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        modalParentBgLight: "#0000007f",
+        textwhite: "#FFFFFF",
+        textdark: "#000112",
+        textgray: "#828FA3",
+        dark_Gray: "#2B2C37",
+        border_mediumGray: "#3E3F4E",
+        border_lightBlue: "#E4EBFA",
+        purple_Dark: "#635FC7",
+        purple_Light: "#A8A4FF",
+        purple_superLight: "#635fc71a",
+        almost_Dark: "#20212C",
+        almost_White: "#F4F7FD",
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+        "xl": '1200px',
+        "sm": '640px',
+        "md": '768px',
+        "xs": "480px",
+        "lg": '960px',
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,36 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
+  darkMode: ['class'],
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+        "xl": '1200px',
+        "sm": '640px',
+        "md": '768px',
+        "xs": "480px",
+        "lg": '960px',
+      },
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        modalParentBgLight: "#0000007f",
+        textwhite: "#FFFFFF",
+        textdark: "#000112",
+        textgray: "#828FA3",
+        border_mediumGray: "#3E3F4E",
+        border_lightBlue: "#E4EBFA",
+
       },
     },
   },
